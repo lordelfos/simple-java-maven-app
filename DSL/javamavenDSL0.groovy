0c6f57aa1060601a52ec2b28c23d09cdd03e3014
lordelfos <lordelfos@gmail.com>
2023-05-26 17:35:47 -0300

    Update and rename javamaven2.groovy to javamavenDSL0.groovy

--- DSL/javamavenDSL0.groovy
+++ DSL/javamavenDSL0.groovy
@@ -1,4 +1,4 @@
-job('Java Maven App DSL 2') {
+job('Java Maven App DSL 0') {
     description('Java Maven App con DSL para el curso de Jenkins')
     scm {
         git('https://github.com/macloujulian/simple-java-maven-app.git', 'master') { node ->
